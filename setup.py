bcaf5fbfc7e21838e6f51cfb21dd21e379ab986f
Vijay Bairagi <vijayvaishnav906@gmail.com>
2025-04-03 14:57:35 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapeer',
+    version= '0.0.1',
+    author= 'vijay',
+    author_email= 'vijayvaishnav906@gmail.com',
+    packages= find_packages(),
+    imstall_requires=[]
+)
